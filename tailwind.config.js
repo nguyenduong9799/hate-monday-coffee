43360180b397e52d1f5bfbaf1a82312697df62b4
nguyenduong971999 <nguyenduong971999@gmail.com>
2024-09-15 08:46:53 +0000

    add font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,21 +2,32 @@
 export default {
 	content: ['./src/**/*.{html,svelte,js,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				sans: ["Space Grotesk", ...tailwind_theme.fontFamily.sans],
+				mono: ['Space Grotesk', ...tailwind_theme.fontFamily.mono],
+
+			  },
+		}
 	},
 	daisyui: {
 		themes: [
 			{
-				mytheme: {
-					primary: '#a991f7',
+				light: {
+					primary: '#9AB676',
 					secondary: '#f6d860',
 					accent: '#37cdbe',
-					neutral: '#3d4451',
+					neutral: '#D0D0D0',
+					warning: '#FCDC2A',
+					error: '#F2613F',
+					success: '#90D26D',
+					info: '#5AB2FF',
+					background: '#FAFBF8',
 					'base-100': '#ffffff'
 				}
 			}
 			// Add more custom themes if needed
 		]
 	},
-	plugins: [require('daisyui')]
+	plugins: [require("@tailwindcss/typography"), require('daisyui')]
 };
