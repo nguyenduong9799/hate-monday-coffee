a15510459315eed38918baf24d69d3ed1bf3859e
Nguyễn Dương <nguyenduong971999@gmail.com>
2024-09-15 16:49:47 +0700

    fix error

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
+	import { goto } from "$app/navigation";
+	import { APP_ROUTES } from "$lib/routes/route";
+	import { onMount } from "svelte";
 
-
-
-
+onMount(()=>{
+    goto(APP_ROUTES.coming_soon);
+})
 
 </script>
-
  <div class="w-full">
  </div>
